fe69df63aabd0234b967fe39d2e2e8234ded3a60
Ionut Ivanciu <ionutziv7@protonmail.com>
2022-07-21 15:10:35 +0300

    If Statements

---
+++ If Statements/script.js
@@ -0,0 +1,11 @@
+var carsLeft = 1,
+  carsRight = 0,
+  greenMan = "no";
+
+if (greenMan == "yes") {
+  console.log("cross the road");
+} else if ((greenMan === "yes" && carsLeft === 0) || carsRight === 0) {
+  console.log("All clear! Cross the road");
+} else {
+  console.log("Stay where you are!");
+}
