a20c143b3cb39402e17e7ca87d69facac8b96794
Ionut Ivanciu <ionutziv7@protonmail.com>
2022-07-21 17:12:32 +0300

    Final Project

---
+++ Final Project/script.js
@@ -0,0 +1,16 @@
+let elements = document.getElementsByName("cssProperty");
+let div = document.getElementById("modify");
+
+function set() {
+  for (let index = 0; index < elements.length; index++) {
+    let cssProperty = elements[index].getAttribute("id");
+
+    let cssValue = elements[index].value;
+
+    let div = document.getElementById("modify");
+
+    div.style[cssProperty] = cssValue;
+  }
+}
+
+document.getElementById("set").addEventListener("click", set);
